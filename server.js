69cb8d261a6c42fe06618e0e6554338d812c45d8
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-09 12:18:04 -0800

    updating notes app

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
 
 app.use(passport.initialize());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('/', function(req, res){
   res.render('index.ejs');
