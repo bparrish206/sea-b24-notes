ed81e555196d069c46cd286bff51e2fdb7477257
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-07-19 22:16:18 -0700

    tweak

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ var bodyparser = require('body-parser');
 var passport = require('passport');
 var app = express();
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/notes_development');
 app.use(bodyparser.json());
 app.use(express.static(__dirname + '/build'));
 app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
 
 app.use(passport.initialize());
-
 require('./lib/passport')(passport);
+
 var jwtauth = require('./lib/jwt_auth')(app.get('jwtSecret'));
 
-//var notesRouter = express.Router();
-//notesRouter.use(jwtauth);
+var notesRouter = express.Router();
+notesRouter.use(jwtauth);
 
-require('./routes/users_routes')(app, passport);
+
 require('./routes/notes_routes')(app);
-//app.use('/v1', app);
+require('./routes/users_routes')(app, passport);
+
+app.use(function(err, req, res, next) {
+ res.status(500).send('error!');
+});
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log('server running on port: %d', app.get('port'));
 });
+
+module.exports = app;
